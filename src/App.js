c06a217f73210066ee5bf3c8cceda36c2fb74384
David Roghanian <dave@fureyfs.com>
2019-11-16 12:33:38 -0500

    hi

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const SNIPPETS = [
+    'Wherefore art thou, JUANHULIO',
+    "Is you is or is you ain't got cash money",
+    'Antidisestablishmentarianism',
+  ]
+  const [snippet, setSnippet] = useState('')
+  const [userText, setUserText] = useState('')
+
+  const chooseSnippet = snippetIndex => () => {
+    console.log('setSnippet', snippetIndex)
+    setSnippet(SNIPPETS[snippetIndex])
+  }
+  const updateUserText = event => {
+    setUserText(event.target.value)
+    console.log('current userText', userText)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Type Race</h2>
+      <hr />
+      <h3>Snippet</h3>
+      {snippet}
+      <input value={userText} onChange={updateUserText} />
+      <hr />
+      {SNIPPETS.map((SNIPPET, index) => {
+        ;<button onClick={chooseSnippet(index)} key={index}>
+          {SNIPPET.substring(0, 10)}...
+        </button>
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
